91323fff8bbd5232c4c62958ec573834635f73c1
perkasalintang52 <98840516+perkasalintang52@users.noreply.github.com>
2022-07-17 13:58:41 +0700

    Update banchat.js

--- plugins/banchat.js
+++ plugins/banchat.js
@@ -1,7 +1,7 @@
 let handler = async (m, { conn, participants }) => {
   // if (participants.map(v=>v.jid).includes(global.conn.user.jid)) {
     global.db.data.chats[m.chat].isBanned = true
-    m.reply('SAD BOTZ')
+    m.reply('*SAD BOTZ*')
   // } else m.reply('Ada nomor host disini...')
 }
 handler.help = ['banchat']
