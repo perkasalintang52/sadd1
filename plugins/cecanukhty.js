e41cf62bed323c88ddc0862080c8f36950d02d07
perkasalintang52 <98840516+perkasalintang52@users.noreply.github.com>
2022-07-19 08:19:41 +0700

    Update cecanukhty.js

--- plugins/cecanukhty.js
+++ plugins/cecanukhty.js
@@ -6,7 +6,7 @@ fetch('https://raw.githubusercontent.com/arivpn/dbase/master/kpop/ukhty.txt')
 let handler = async (m, { conn }) => {
     let img = zzz[Math.floor(Math.random() * zzz.length)]
     if (!img) throw img
-    await conn.sendFile(m.chat, img, '', '✿𝗖𝗥𝗘𝗔𝗧𝗢𝗥➢𝐆𝐞𝐦𝐩𝐲 𝐓𝐨𝐧\nSubscribe : https://www.youtube.com/channel/UCZoVmApPxtLYgUWwBD4nbCw', m, 0, { thumbnail: await (await fetch(img)).buffer() })
+    await conn.sendFile(m.chat, img, '', '✿𝗖𝗥𝗘𝗔𝗧𝗢𝗥➢Lintang\nSubscribe : https://tiktok.com/@joceylintangg', m, 0, { thumbnail: await (await fetch(img)).buffer() })
 }
 handler.help = ['cecanukhty']
 handler.tags = ['asupan']
