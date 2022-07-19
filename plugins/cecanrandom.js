c1601e07ba489e951fdbc26adad5d8ed53278cf6
perkasalintang52 <98840516+perkasalintang52@users.noreply.github.com>
2022-07-19 08:12:55 +0700

    Update cecanrandom.js

--- plugins/cecanrandom.js
+++ plugins/cecanrandom.js
@@ -6,7 +6,7 @@ fetch('https://raw.githubusercontent.com/arivpn/dbase/master/kpop/cecan.txt')
 let handler = async (m, { conn }) => {
     let img = arr[Math.floor(Math.random() * arr.length)]
     if (!img) throw img
-    await conn.sendFile(m.chat, img, '', '✿𝗖𝗥𝗘𝗔𝗧𝗢𝗥➢𝐆𝐞𝐦𝐩𝐲 𝐓𝐨𝐧\nSubscribe : https://www.youtube.com/channel/UCZoVmApPxtLYgUWwBD4nbCw', m, 0, { thumbnail: await (await fetch(img)).buffer() })
+    await conn.sendFile(m.chat, img, '', '✿𝗖𝗥𝗘𝗔𝗧𝗢𝗥➢Lintang\nSubscribe : https://tiktok.com/@joceylintangg', m, 0, { thumbnail: await (await fetch(img)).buffer() })
 }
 handler.help = ['cecanrandom']
 handler.tags = ['asupan']
