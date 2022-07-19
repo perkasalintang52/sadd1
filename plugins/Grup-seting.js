d0c5e7be1e0c9606ce50671d4e61f001a1fa853f
perkasalintang52 <98840516+perkasalintang52@users.noreply.github.com>
2022-07-19 08:25:15 +0700

    Update Grup-seting.js

--- plugins/Grup-seting.js
+++ plugins/Grup-seting.js
@@ -1,19 +1,19 @@
 let handler = async (m, { conn, args, usedPrefix, command }) => {
     let isClose = { // Switch Case Like :v
-        'open': 'not_announcement',
-        'close': 'announcement',
+        'b': 'not_announcement',
+        't': 'announcement',
     }[(args[0] || '')]
     if (isClose === undefined)
         throw `
 *Format salah! Contoh :*
-  *○ ${usedPrefix + command} close*
-  *○ ${usedPrefix + command} open*
+  *○ ${usedPrefix + command} tutup (t)*
+  *○ ${usedPrefix + command} buka (b)*
 `.trim()
     await conn.groupSettingUpdate(m.chat, isClose)
 }
-handler.help = ['group *open / close*']
+handler.help = ['gc *b / t*']
 handler.tags = ['group']
-handler.command = /^(group)$/i
+handler.command = /^(gc)$/i
 
 handler.admin = true
 handler.botAdmin = true
